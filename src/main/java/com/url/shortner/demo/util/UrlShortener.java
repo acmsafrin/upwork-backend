d9f67e590c64f315aecdcdff0a7adf4f94c9098e
safrin <acmsafri@gmail.com>
2023-04-13 14:49:54 +0800

    Add salt

--- src/main/java/com/url/shortner/demo/util/UrlShortener.java
+++ src/main/java/com/url/shortner/demo/util/UrlShortener.java
@@ -1,6 +1,7 @@
 package com.url.shortner.demo.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 
 @Slf4j
 public class UrlShortener {
@@ -10,13 +11,17 @@ public class UrlShortener {
     private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     private static final long BASE = ALPHABET.length();
 
+
+    @Value("${token.secret}")
+    private static String salt;
+
     /**
      * There is chance for collision
      * @param longUrl
      * @return
      */
     public static String shorten(String longUrl) {
-        int hashCode = longUrl.hashCode();
+        int hashCode = (longUrl+salt).hashCode();
         final long OFFSET = Long.MAX_VALUE;
         long modifed=Math.abs(hashCode+OFFSET);
         log.info("URL"+longUrl+"Hashcode "+hashCode+"::"+modifed);
